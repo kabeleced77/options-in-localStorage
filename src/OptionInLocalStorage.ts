64eae8f21652286f4cd52af13452cd377c517b81
kabeleced77 <kabeleced@arcor.de>
2021-10-03 20:54:20 +0200

    refactor: added more comments on methods; renamed internal methods

--- src/OptionInLocalStorage.ts
+++ src/OptionInLocalStorage.ts
@@ -8,32 +8,42 @@ import { StorageValue } from './StorageValue'
  */
 export class OptionInLocalStorage implements IOptionAsync {
   /**
-   * Creates an object holding the name and - if given - default value of the option. Actual access is provided through the object's correspondent methods. 
-   * @param optionName name of option. Can be used alone providing access to the stored value. If there is no value stored the given or standard-default value is stored.
-   * @param defaultValue default value of the option. This is optional. If not given standard-default value is used otherwise the given one.
+   * Creates an object holding the name and - if given - default value of the option. Actual access is provided through the object's correspondent methods.
+   *
+   * If no default value is given an internal value is used instead.
+   * @param optionName name of option. Can be used alone providing access to the stored value. If there is no value stored the given or internal default value is stored.
+   * @param [defaultValue] default value of the option. This is optional. If not given an internal default value is used instead.
    */
   constructor(private optionName: string, private defaultValue: string = '') {}
   /**
-   * Provides the name of the option. Actually the same value given through construction of object.
+   * Provides the name of the option. Actually the same value given through constructor.
    * @returns the name of the option.
    */
   public name(): string {
     return this.optionName
   }
   /**
-   * 
-   * @returns the actual value of the option. Either the saved one or the - if given default value - if nothing has been saved so far. The 
+   * Provides the actual value of an option saved in local storage. If there is no value saved at all the default value is returned.
+   *
+   * @returns actual value of the option. Either the saved one or the default value - if nothing has been saved so far.
    */
   public async value(): Promise<string> {
     return (await this.getStorageValue(this.optionName, this.defaultValue)).value()
   }
   /**
+   * Provides the default value of the option.
    *
-   * @returns default value save in storage; if nothing has been saved so far given default value is saved and returned.
+   * @returns default value of the option.
    */
   public async default(): Promise<string> {
     return (await this.getStorageValue(this.optionName, this.defaultValue)).default()
   }
+  /**
+   * Reset the actual value of the option to its default value.
+   *
+   * If no option has been saved so far, it will be now using default value.
+   * @returns Promise of void.
+   */
   public async reset(): Promise<void> {
     const storageDefaultValue = (
       await this.getStorageValue(this.optionName, this.defaultValue)
@@ -43,6 +53,11 @@ export class OptionInLocalStorage implements IOptionAsync {
       new StorageValue(storageDefaultValue, storageDefaultValue),
     )
   }
+  /**
+   * Updates the actual value of the option by the value returned by the given callback. If no option hase been saved so far, it will be now using that returned value.
+   * @param updateFunction callback which provides the current value of the options. It must return the new value of the option
+   * @returns Promise of void.
+   */
   public async update(updateFunction: (currentValue: string) => string): Promise<void> {
     const storageValue = await this.getStorageValue(this.optionName, this.defaultValue)
     return this.toLocalStorage(
@@ -50,19 +65,27 @@ export class OptionInLocalStorage implements IOptionAsync {
       new StorageValue(updateFunction(storageValue.value()), storageValue.default()),
     )
   }
+  /**
+   * Provides value from localStorage referenced by given name of option. If nothing has been saved so far the default value is used.
+   * @param optionName name of option the storageValue is saved by.
+   * @param defaultValue default value of option
+   * @returns Promise of IStorageValue which holds the actual and default value of the option.
+   */
   private async getStorageValue(optionName: string, defaultValue: string): Promise<IStorageValue> {
-    let storageValue: IStorageValue
     const fromStorage = (await this.localStorage().get(optionName))[optionName]
     // has option been saved at all?
-    if (fromStorage) {
-      // yes -> return saved value
-      storageValue = new StorageValue().fromJson(fromStorage)
-    } else {
-      // no -> return default value and save default value in storage
-      storageValue = new StorageValue(defaultValue, defaultValue)
-    }
-    return storageValue
+    return fromStorage
+      ? // yes -> return saved value
+        new StorageValue().fromJson(fromStorage)
+      : // no -> return default value and save default value in storage
+        new StorageValue(defaultValue, defaultValue)
   }
+  /**
+   * Saves storageValue in localStorage referenced by given name. 
+   * @param optionName name of option the storageValue will be saved by in the localStorage.
+   * @param storageValue the value saved in localStorage referenced by name.
+   * @returns Promise of void.
+   */
   private async toLocalStorage(optionName: string, storageValue: IStorageValue): Promise<void> {
     return this.localStorage().set({ [optionName]: storageValue })
   }
--- src/OptionsInLocalStorage.ts
+++ src/OptionsInLocalStorage.ts
@@ -6,25 +6,39 @@ import { StorageValue } from './StorageValue'
 
 export class OptionsInLocalStorage implements IOptionsAsync {
   constructor() {}
+  /**
+   * Get all options.
+   * @returns array of all options in local storage.
+   */
   public async options(): Promise<IOptionAsync[]> {
     return Object.keys(await this.localStorage().get(null)).map(
       (option) => new OptionInLocalStorage(option),
     )
   }
+  /**
+   * Get value of option referenced by given name from local storage. In case there is no value saved, it will be saved using given default value. If there is no default value given, internal standard default value is used.
+   * @param name name of option whose referenced value is required from local storage.
+   * @param defaultValue default value used in case there is no value saved yet. Option will then be saved having this default value.
+   * @returns Promise with saved storage value referenced by given name.
+   */
   public async option(name: string, defaultValue?: string): Promise<IOptionAsync> {
+    const storageValue = await this.getOrSetStorageValue(name, defaultValue ? defaultValue : '')
     return defaultValue
-      ? new OptionInLocalStorage(
-          name,
-          (await this.getOrSetStorageValue(name, defaultValue)).default(),
-        )
+      ? new OptionInLocalStorage(name, storageValue.default())
       : new OptionInLocalStorage(name)
   }
+  /**
+   * Get value from local storage saved by given name of option. If there is no value save in storage, it will be save using given default value.
+   * @param optionName name of option value is save by in local storage.
+   * @param defaultValue default value used to initially save option value in local storage by given name.
+   * @returns Promise with saved storage value referenced by given name.
+   */
   private async getOrSetStorageValue(
     optionName: string,
     defaultValue: string,
   ): Promise<IStorageValue> {
     let storageValue: IStorageValue
-    const fromStorage = (await this.localStorage().get(optionName))[optionName]
+    const fromStorage = (await this.fromLocalStorage(optionName))[optionName]
     // has option been saved at all?
     if (fromStorage) {
       // yes -> return saved value
@@ -36,8 +50,22 @@ export class OptionsInLocalStorage implements IOptionsAsync {
     }
     return storageValue
   }
+  /**
+   * Put storageValue into local storage saved by optionName.
+   * @param optionName name of option the actual value shall be saved by in local storage.
+   * @param storageValue content save to storage.
+   * @returns
+   */
   private async toLocalStorage(optionName: string, storageValue: IStorageValue): Promise<void> {
     return this.localStorage().set({ [optionName]: storageValue })
+  }
+  /**
+   * Get storageValue from local storage saved by optionName.
+   * @param optionName name of option which is used to get actual value from local storage.
+   * @returns promise of an object which should be saved in local storage by optionName.
+   */
+  private async fromLocalStorage(optionName: string): Promise<Record<string, {}>> {
+    return this.localStorage().get(optionName)
   }
   /**
    * Check and return localStorage. If not available in current context throw exception.
